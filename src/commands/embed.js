31d2d05f3f44a47999c9b2e865b68d457b7a71bb
Scoffgard <40467322+Scoffgard@users.noreply.github.com>
2018-06-22 20:49:44 +0200

    Update embed.js

--- src/commands/embed.js
+++ src/commands/embed.js
@@ -1,9 +1,8 @@
 exports.run = (client, message, args) => {
     args = args.join(' ');
     message.delete();
-    if(!message.member.roles.some(r=>["Arrière-Grand-Père"].includes(r.name)) )
-      return message.channel.send("Désolé, vous n'avez pas le droit de faire ca!")
-    let message.channel.send({
+    if(role.hasPermission('checkAdmin')){
+    message.channel.send({
         embed: {
             author: {
                 name: `Message par ${message.author.username}`,
@@ -12,5 +11,7 @@ exports.run = (client, message, args) => {
             color: 3447003,
             description: args
         }
-    });
+    })}else{
+message.channel.send("Vous n'avez pas le droit de faire ca !)
+    };
 }
