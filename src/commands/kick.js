fedd66a0d49c0d29cfae6ef5635f59292351d46b
ShiroanXyz <40276172+ShiroanXyz@users.noreply.github.com>
2018-06-24 12:24:18 +0200

    Update kick.js

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
     user = message.mentions.users.first();
     message.delete();
     if (message.member.hasPermission("KICK_MEMBERS")){
-        member.kick(user);
+        user.kick();
         message.channel.send(`**${user.tag}** a été kick!`);
     } else {
         message.author.send("Vous n'avez pas le droit d'executer la commande \"kick\" !");
